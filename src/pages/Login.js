d43589f48ca647285c97c1d900ac8b204fc27810
Thiago Marchini <thiago@msn.com>
2021-06-08 16:05:16 -0300

    feita a verificação dos campos de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,67 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+
+    this.state = {
+      email: undefined,
+      password: undefined,
+      isDisabled: true,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    const { email, password } = this.state;
+    const minPasswordLength = 6;
+    if (((name === 'email') && (this.validateEmail(value)))
+    || ((name === 'password') && (value.length >= minPasswordLength))) {
+      this.setState({
+        [name]: value,
+      });
+    }
+    if (email && password) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  }
+
+  // Função de validação de email com regex retirada de: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
+  validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
   render() {
+    const { isDisabled } = this.state;
+    console.log(this.state);
     return (
       <form>
         <label htmlFor="email">
           E-mail:
-          <input type="email" name="email" data-testid="email-input" />
+          <input
+            type="email"
+            name="email"
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
         </label>
         <label htmlFor="password">
           Senha:
-          <input type="password" name="password" data-testid="password-input" />
+          <input
+            type="password"
+            name="password"
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
         </label>
-        <button type="submit">Entrar</button>
+        <button type="submit" disabled={ isDisabled }>Entrar</button>
       </form>
     );
   }
